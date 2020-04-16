5926821937141bba40c38b952fc0f4fe7e7afd6b
Quentin Renard <quentin@area17.com>
2020-04-16 02:48:13 -0400

    Integrate calendar and event routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::name('home')->get('/', function () {
     return view('site.welcome');
@@ -26,9 +26,31 @@ Route::name('members')->get('/our-members', function () {
 });
 
 Route::name('calendar')->get('/calendar', function () {
-    return view('site.calendar');
+
+    $upcomingEvents = \App\Models\Event::published()->where('date', '>=', \Carbon\Carbon::today())->get();
+    $pastEvents = \App\Models\Event::published()->where('date', '<', \Carbon\Carbon::today())->get();
+
+    return view('site.calendar', [
+        'upcomingEvents' => $upcomingEvents,
+        'pastEvents' => $pastEvents,
+    ]);
 });
 
-Route::name('event')->get('/event', function () {
-    return view('site.event');
+Route::name('event.show')->get('/{locale}/events/{slug}', function ($locale, $slug) {
+
+    if (in_array($locale, config('translatable.locales'))) {
+        app()->setLocale($locale);
+    }
+
+    $event = app(\App\Repositories\EventRepository::class)->forSlug($slug);
+
+    abort_unless($event, 404);
+
+    if ($event->redirect) {
+        return redirect()->route('event.show', ['locale' => $locale, 'slug' => $event->slug]);
+    }
+
+    return view('site.event', [
+        'item' => $event,
+    ]);
 });
